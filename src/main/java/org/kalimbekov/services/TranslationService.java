87d730322a61e29d31122390ffccad7dcfee3995
ssstroke <capslk@tuta.io>
2023-05-30 13:11:29 +0300

    Remove detectLanguage()

--- src/main/java/org/kalimbekov/services/TranslationService.java
+++ src/main/java/org/kalimbekov/services/TranslationService.java
@@ -64,33 +64,6 @@ public class TranslationService {
         return service.createChatCompletion(chatCompletionRequest).getChoices().get(0).getMessage().getContent();
     }
 
-    public String detectLanguage(String originalText) {
-        final String SYSTEM_TASK_MESSAGE = """
-                You are a part of application for studying foreign languages. I will give you a text for you to
-                detect a language. You should detect a language of original text and your confidence in the answer
-                (confidence must be a percent value). You should respond in a specific format:
-                Язык текста: <language you have detected in Russian language>
-                Вероятность правильного определения: <your confidence>
-                """
-                ;
-
-        OpenAiService service = new OpenAiService(this.OPENAI_API_TOKEN);
-
-        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
-                .builder()
-                .model("gpt-3.5-turbo")
-                .temperature(0.7)
-                .messages(
-                        List.of(
-                                new ChatMessage("system", SYSTEM_TASK_MESSAGE),
-                                new ChatMessage("user", originalText)
-                        )
-                )
-                .build();
-
-        return service.createChatCompletion(chatCompletionRequest).getChoices().get(0).getMessage().getContent();
-    }
-
     public String checkTranslation(String originalText, String translatedText) {
         final String SYSTEM_TASK_MESSAGE = """
                 You are a part of application for studying foreign languages. I will give you two texts: first one is
